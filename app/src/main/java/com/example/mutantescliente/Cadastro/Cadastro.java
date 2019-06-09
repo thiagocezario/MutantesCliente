39cb28e7f6deac62a01689fea480c6b58842ee09
Thiago Cezario <thiagocezario2@gmail.com>
2019-06-09 19:43:39 -0300

    Add action to image view on new mutant screen

--- app/src/main/java/com/example/mutantescliente/Cadastro/Cadastro.java
+++ app/src/main/java/com/example/mutantescliente/Cadastro/Cadastro.java
@@ -1,15 +1,76 @@
 package com.example.mutantescliente.Cadastro;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.mutantescliente.R;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class Cadastro extends AppCompatActivity {
+
+    private ImageView mutantPhoto;
+    public static final int IMAGE_GALLERY_REQUEST = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        mutantPhoto = findViewById(R.id.mutantPhoto);
+
+        setupListeners();
+    }
+
+    private void setupListeners() {
+        mutantPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+
+                File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                String pictureDirectoryPath = pictureDirectory.getPath();
+
+                Uri data = Uri.parse(pictureDirectoryPath);
+
+                photoPickerIntent.setDataAndType(data, "image/*");
+
+                startActivityForResult(photoPickerIntent, IMAGE_GALLERY_REQUEST);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == IMAGE_GALLERY_REQUEST) {
+                Uri imageUri = data.getData();
+
+                InputStream inputStream;
+
+                try {
+                    inputStream = getContentResolver().openInputStream(imageUri);
+
+                    Bitmap image = BitmapFactory.decodeStream(inputStream);
+
+                    mutantPhoto.setImageBitmap(image);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, "Não foi possível abrir a galeria", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
